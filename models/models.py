687aad273b665667af58040bd714f2b6ca07d212
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2024-10-03 12:09:26 -0300

    Initialized empty module

---
+++ models/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class MrpUnbuild(models.Model):
+    _inherit = "mrp.unbuild"
+    
+    
+# class my_module(models.Model):
+#     _name = 'my_module.my_module'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
